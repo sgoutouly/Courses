46d0303c5cd01f795066b16bb1de43f6f5106ca0
sgoutouly <sgoutouly@gmail.com>
2014-04-23 21:40:54 +0200

    Ajout Jsroutes Play dans la partie serveur et cliente, modification chat pour afficher la liste des personnes connectées ainsi qu'exclure des notifications l'expéditeur. modification grunt en cours pour les livraisons.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,22 +2,40 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
+    uglify: {
+      build: {
+        src: ['public/javascripts/app/*.js'],
+        dest: 'public/javascripts/lib/main.js'
+      }
+    },
+
     watch: {
       options: {
         livereload: true
       },
       app: {
-        files: ['public/*.*', 'app/views/*.*']
+        files: ['public/*.*', 'app/views/*.html']
       },
       scripts: {
-        files: ['public/**/*.*', 'public/**/**/*.*', 'public/**/**/**/*.*']
+        files: ['public/javascripts/app/*.js'],
+        tasks: ['uglify']
+      }
+    },
+
+    copy: {
+      all: {
+        files: [
+          {expand: true, src: ['client/**'], dest: 'target'}
+        ]
       }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
   grunt.registerTask('default', ['watch']);
-  
 
 };
